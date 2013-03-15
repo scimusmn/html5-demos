46f5ca16845a56cd41fc1aa295654ff905032192
Sarah German <sgerman@smm.org>
2013-03-15 12:12:13 -0500

    Function to reset draggable positions

--- js/main.js
+++ js/main.js
@@ -11,7 +11,31 @@ $(document).ready(function() {
 
   // Allow items with the "dragme" class to be draggable
   // This requires jQueryUI (see http://jqueryui.com/draggable)
-  $('.dragme').draggable();
+  $draggable = $('.dragme'); // Set this as a variable. Since we use it multiple times, this helps performance
+  $draggable.draggable(); // Initialize draggable functionality
+
+  // Make note of each draggable's original position, and store them as data on the objects.
+  // Will use for resetting.
+  $.each($draggable, function() {
+    var top = $(this).offset().top,
+        left = $(this).offset().left;
+    $(this).data('top', top).data('left', left);
+  });
+
+  // Reset positions on draggables when the button is clicked
+  $('#drag-reset').click(function() {
+    $.each($draggable, function() {
+      $(this).css({
+        'top': $(this).data('top')
+      });
+      $(this).css({
+        'position': 'absolute'
+      });
+      $(this).css({
+        'left': $(this).data('left')
+      });
+    });
+  });
 
   // Play and pause buttons under the video
   var v = document.getElementById('roxanne-video');
