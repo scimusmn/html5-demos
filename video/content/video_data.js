2c1bbe0bcc597c79cff83f9a419ccbc0e6a2f5ea
Sarah German <sgerman@smm.org>
2013-03-19 16:20:38 -0500

    Added more demo content and a grid

--- video/content/video_data.js
+++ video/content/video_data.js
@@ -4,9 +4,19 @@
 */
 var data = {
   PageTitle : "This is an example video kiosk",
-  IntroText: "",
+  IntroText: "Here's some sample intro text. Roxanne Gould is Grand Traverse and Little Traverse Bands Odawa and Ojibwe of Michigan. Roxanne is an educator and a scholar working on Indigenous educational design. In 2014 she and others will open the Badote Learning Center a place-based, Dakota/Ojibwe language immersion school.",
   Videos : [
-    { Name : "The revitalization of Indigenous language is happening across the world", File : "01_0104_Roxanne.mp4", Image : "roxanne1.jpg" },
-    { Name : "The last first language speaker", File : "02_0104_Roxanne.mp4", Image : "roxanne2.jpg" }
+    {
+      Name : "The revitalization of Indigenous language is happening across the world",
+      File : "01_0104_Roxanne.mp4",
+      Image : "roxanne1.jpg",
+      Summary : "Dapibus ac mauris et dis lorem augue auctor? Habitasse habitasse. Arcu nec tincidunt nisi, auctor magna, aenean? Parturient, ultricies enim. In! Integer elementum, pulvinar! Placerat in massa nisi tempor et, nunc odio, egestas hac purus turpis, quis!"
+    },
+    {
+      Name : "The last first language speaker",
+      File : "02_0104_Roxanne.mp4",
+      Image : "roxanne2.jpg",
+      Summary : "Odio in integer aliquet in scelerisque nec, placerat porttitor vel a, augue mid. Pellentesque facilisis hac scelerisque nec placerat mid quis ac mauris scelerisque porttitor facilisis habitasse, aenean, integer non aliquet? Hac eros, lundium, elementum nascetur habitasse, sit pulvinar enim urna! Velit arcu."
+    }
   ]
 };
