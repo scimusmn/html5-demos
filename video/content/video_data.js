12674602cd90d939c13c2aa3d577f8abf382eac7
Sarah German <sgerman@smm.org>
2013-03-19 14:02:59 -0500

    Separated content template from page template

---
+++ video/content/video_data.js
@@ -0,0 +1,12 @@
+/*
+ * This array is passed to the page template, ../templates/video.handlebars,
+ * which renders it into the appropriate HTML elements.
+*/
+var data = {
+  PageTitle : "This is an example video kiosk",
+  IntroText: "",
+  Videos : [
+    { Name : "The revitalization of Indigenous language is happening across the world", File : "01_0104_Roxanne.mp4", Image : "roxanne1.jpg" },
+    { Name : "The last first language speaker", File : "02_0104_Roxanne.mp4", Image : "roxanne2.jpg" }
+  ]
+};
