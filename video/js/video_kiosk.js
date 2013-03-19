9ec908598fe3aaa9cf36521fe2954d0aadb7f2f2
Sarah German <sgerman@smm.org>
2013-03-19 15:36:10 -0500

    Show video poster image when video ends

--- video/js/video_kiosk.js
+++ video/js/video_kiosk.js
@@ -11,18 +11,26 @@ function video_kiosk() {
       videoIDs.push(this.id);
     });
 
-    // Initialize the videojs plugin. Usually automated, but needed here since the video markup is created with AJAX
-    // https://github.com/zencoder/video-js/blob/master/docs/setup.md#alternative-setup-for-dynamically-loaded-html
-    $.each(videoIDs, function(i, val) {
-      _V_(val, {}, function(){});
+    // Apply some extra functions to each one
+    $.each(videoIDs, function(index, videoID) {
+
+      // Initialize the videojs plugin on each video on the page.
+      // Usually automated, but needed here since the video markup is created with AJAX
+      // https://github.com/zencoder/video-js/blob/master/docs/setup.md#alternative-setup-for-dynamically-loaded-html
+      _V_(videoID, {}, function(){});
+
+      // Reset poster image when video ends
+      _V_(videoID).ready(function() {
+        this.addEvent("ended", function() {
+          this.posterImage.show();
+        });
+      });
+
+      // @TODO Stop a playing video if another is started
+
+      // @TODO Start/pause video from clicking on its associated content
+
+      // @TODO Use modal window for full-screen - easier to close on kiosk
+
     });
-
-  // @TODO Stop a playing video if another is started
-
-  // @TODO Start/pause video from clicking on its associated content
-
-  // @TODO Use modal window for full-screen - easier to close on kiosk
-
-  // @TODO Reset poster image when video ends
-
 };
