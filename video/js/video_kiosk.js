60b4add21b5e92378e7da9ac9656417af2a94a96
Sarah German <sgerman@smm.org>
2013-03-19 14:45:02 -0500

    Starter file for JS-controlled video playback

---
+++ video/js/video_kiosk.js
@@ -0,0 +1,16 @@
+/*
+ * Add-on functionality for video.js
+ * Extra features for better playback on an exhibit kiosk.
+*/
+
+$(function() {
+
+  // @TODO Stop a playing video if another is started
+
+  // @TODO Start/pause video from clicking on its associated content
+
+  // @TODO Use modal window for full-screen - easier to close on kiosk
+
+  // @TODO Reset poster image when video ends
+
+});
