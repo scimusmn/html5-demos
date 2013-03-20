1890df60766f2f62e6655249f9d593eae881746f
Sarah German <sgerman@smm.org>
2013-03-20 16:57:16 -0500

    Rewind video is another is selected

--- video/js/video_kiosk.js
+++ video/js/video_kiosk.js
@@ -6,7 +6,7 @@
 function video_kiosk() {
 
   // Set some variables, we'll use these a bunch
-  var $videoOption = $('#video-options div');
+  var $videoOption = $('#video-options div').not('.selected');
   var videoPlayer = _V_('videoPlayer');
   // Path is relative to the kiosk page.
   // Should contain subdirectories for images and videos.
@@ -15,8 +15,6 @@ function video_kiosk() {
   $videoOption.eq(0).addClass('selected'); // Select the first video initially
 
   // Initialize the videojs plugin.
-  // Usually automated, but needed here since the video markup is created with AJAX
-  // https://github.com/zencoder/video-js/blob/master/docs/setup.md#alternative-setup-for-dynamically-loaded-html
   videoPlayer.ready(function() {
 
     // Reset poster image when video ends
@@ -24,25 +22,30 @@ function video_kiosk() {
       this.posterImage.show();
     });
 
-    // @TODO Enlarge video on play, size back to normal on pause/end. Hide summary.
-    // @TODO Use modal window for full-screen - easier to close on kiosk
-
-  });
-
-    // Toggle video play/pause by clicking on its title or poster image
+    // Toggle main video by clicking on its title or poster image
     $videoOption.click(function() {
       var video = $(this).attr('data-video-source');
       var poster = $('img', this).attr('src');
 
       // Change the video source and poster attributes
-      videoPlayer.src(videoPath + video);
-      $('.vjs-poster').attr('src', poster);
+      if (!$(this).hasClass('selected')) {
+        videoPlayer.src(videoPath + video);
+        $('.vjs-poster').attr('src', poster);
 
-      // Add the selected class to the selected video
-      $('.selected').removeClass('selected');
-      $(this).addClass('selected');
+        // Be kind, rewind
+        videoPlayer.currentTime("0").posterImage.show();
+        $('.vjs-big-play-button').css('visibility','visible').show();
+
+        // Add the selected class to the selected video
+        $('.selected').removeClass('selected');
+        $(this).addClass('selected');
+      }
     });
 
+    // @TODO Enlarge video on play, size back to normal on pause/end. Hide summary.
+    // @TODO Use modal window for full-screen - easier to close on kiosk
+
+  });
 
 };
 
