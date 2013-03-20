450a7d4dcacb003281859a605ed78507e19ba142
Sarah German <sgerman@smm.org>
2013-03-20 15:40:34 -0500

    Visually indicating selected video

--- video/js/video_kiosk.js
+++ video/js/video_kiosk.js
@@ -5,20 +5,36 @@
 
 function video_kiosk() {
 
+  // Set some variables, we'll use these a bunch
+  var $videoOption = $('#video-options div');
+  var videoPlayer = _V_('videoPlayer');
+
+  $videoOption.eq(0).addClass('selected'); // Select the first video initially
+
   // Initialize the videojs plugin.
   // Usually automated, but needed here since the video markup is created with AJAX
   // https://github.com/zencoder/video-js/blob/master/docs/setup.md#alternative-setup-for-dynamically-loaded-html
-  _V_('videoPlayer').ready(function() {
+  videoPlayer.ready(function() {
 
     // Reset poster image when video ends
     this.addEvent("ended", function() {
       this.posterImage.show();
     });
 
-    // @TODO Start/pause video from clicking on its associated content
+    // @TODO Enlarge video on play, size back to normal on pause/end. Hide summary.
     // @TODO Use modal window for full-screen - easier to close on kiosk
 
   });
+
+    // Toggle video play/pause by clicking on its title or poster image
+    $videoOption.click(function() {
+      var id = this.id;
+      // Change the video source and poster attributes
+
+      // Add the selected class to the selected video
+      $('.selected').removeClass('selected');
+      $('#' + id).addClass('selected');
+    });
 
 
 };
